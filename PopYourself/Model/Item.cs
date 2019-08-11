f7090cc2b1aa0655aaaa216cd1b46c98e8312d10
cyala <alatraca@sheridancollege.ca>
2019-08-10 21:33:19 -0400

    added item class

---
+++ PopYourself/Model/Item.cs
@@ -0,0 +1,21 @@
+﻿//AUTHOR: Cyrus Alatraca
+//ID: 991146084
+//DATE: July 17, 2019
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PopYourself.Model
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string City { get; set; }
+        public string Phone { get; set; }
+        public decimal Price { get; set; }
+    }
+}
