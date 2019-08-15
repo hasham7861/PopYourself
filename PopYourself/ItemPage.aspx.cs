1465488533306acea685f116ca34ae662abb7f88
RobertSarmiento <sarmirob@sheridancollege.ca>
2019-08-14 23:08:02 -0400

    Final

--- PopYourself/ItemPage.aspx.cs
+++ PopYourself/ItemPage.aspx.cs
@@ -15,13 +15,15 @@ namespace PopYourself
         //pass in the item information from the browse.aspx page
         //display the result to itempage
         protected void Page_Load(object sender, EventArgs e)
-        {
+        {   
+
             BrowsePageDBUtil db = new BrowsePageDBUtil();
 
             string id = (string)Request.Params.Get("id");
             Item item = db.GetItem(id);
 
             name.Text = item.Name;
+            
             price.Text = "$"+item.Price;
             desc.Text = item.Description;
         }
