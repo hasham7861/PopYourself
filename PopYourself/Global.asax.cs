5d7390037f51cc9f7b83d34a489e2cca8d9e4900
hasham7861 <hasham.alam@hotmail.com>
2019-08-17 21:31:26 -0400

    removed unnesscarry bloatware js scripts

--- PopYourself/Global.asax.cs
+++ PopYourself/Global.asax.cs
@@ -15,7 +15,6 @@ namespace PopYourself
         {
             // Code that runs on application startup
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
